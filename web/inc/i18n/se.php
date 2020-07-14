b3f6f55ed1b800721bf6306cecd5fa8649ededad
blockademarc <blockademarc@isbnet.com>
2020-07-14 19:54:37 +0200

    Update se.php
    added ftps

--- web/inc/i18n/se.php
+++ web/inc/i18n/se.php
@@ -572,6 +572,7 @@ $LANG['se'] = array(
     'Remote backup' => 'Remote backup',
     'ftp' => 'FTP',
     'sftp' => 'SFTP',
+	'ftps' => 'FTPS',
     'SFTP Chroot' => 'SFTP Chroot',
     'FileSystem Disk Quota' => 'FileSystem Disk Quota',
     'Hestia Control Panel Plugins' => 'Plugins',
