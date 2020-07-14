addb611eacb7e98e6f71e30062d2d7b0925e507a
blockademarc <blockademarc@isbnet.com>
2020-07-14 19:48:50 +0200

    Update fi.php
    added ftps

--- web/inc/i18n/fi.php
+++ web/inc/i18n/fi.php
@@ -574,6 +574,7 @@ $LANG['fi'] = array(
     'Remote backup' => 'Remote backup',
     'ftp' => 'FTP',
     'sftp' => 'SFTP',
+	'ftps' => 'FTPS',
     'SFTP Chroot' => 'SFTP Chroot',
     'FileSystem Disk Quota' => 'FileSystem Disk Quota',
     'Hestia Control Panel Plugins' => 'Plugins',
